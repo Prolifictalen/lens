42746ba1ed2ee608ca0316d96a57171dcb59ace5
Astha Singh <95242101+Prolifictalen@users.noreply.github.com>
2024-12-08 21:11:39 +0530

    Update Home1.js

--- src/Home1.js
+++ src/Home1.js
@@ -12,26 +12,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faShoppingCart, faPhone } from "@fortawesome/free-solid-svg-icons";
 import lenskart from "./img/lenskart.png";
 import LoginModal from "./Components/LoginModal";
-import axios from "axios";
 
 const Home1 = () => {
   const [show, setShow] = useState(false);
+  const [searchItem, setSearchItem] = useState("");
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const [searchItem, setSearchItem] = useState("");
   const search = async () => {
-    const data = await axios.get(`http://localhost:8000/api/v1/user/getitems/:${searchItem}`);
-    console.log(data.data);
+    // Simulate API call
+    console.log(`Searching for: ${searchItem}`);
     setSearchItem("");
   };
 
   return (
     <>
-     <Navbar bg="white" variant="light" expand="lg" className="py-3">
+      {/* Top Navbar */}
+      <Navbar bg="white" className="py-2">
+        <Container fluid className="justify-content-end">
+          <Nav>
+            <Nav.Link href="contactus" style={{ color: "#007BFF", fontSize: "12px" }}>
+              Contact Us
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      {/* Main Navbar */}
+      <Navbar bg="white" variant="light" expand="lg" className="py-3">
         <Container fluid>
           <Navbar.Brand>
-            <img src={lenskart} alt="Lenskart Logo" style={{ width: "150px", height: "auto" }} />
+            <img src={lenskart} alt="Lenskart Logo" style={{ width: "150px" }} />
           </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link className="d-flex align-items-center">
@@ -48,8 +59,7 @@ const Home1 = () => {
                   onChange={(e) => setSearchItem(e.target.value)}
                   placeholder="What are you looking for?"
                   className="me-2"
-                  aria-label="Search"
-                  style={{ width: "300px", fontSize: "16px", height: "40px" }}
+                  style={{ width: "300px", fontSize: "16px" }}
                 />
                 <Button variant="outline-success" onClick={search}>Search</Button>
               </InputGroup>
@@ -58,11 +68,11 @@ const Home1 = () => {
           <Nav>
             <Nav.Link href="trackorder">Track Order</Nav.Link>
             <Button variant="white" onClick={handleShow}>Login/Register</Button>
-            <Nav.Link href="wishlist" className="d-flex align-items-center">
+            <Nav.Link href="wishlist">
               <FontAwesomeIcon icon={faHeart} />
               <span className="ms-1">Wishlist</span>
             </Nav.Link>
-            <Nav.Link href="cart" className="d-flex align-items-center">
+            <Nav.Link href="cart">
               <FontAwesomeIcon icon={faShoppingCart} />
               <span className="ms-1">Cart</span>
             </Nav.Link>
@@ -70,45 +80,26 @@ const Home1 = () => {
         </Container>
       </Navbar>
 
-      <Navbar bg="Blue" data-bs-theme="light" className="py-2">
+      {/* Categories Navbar */}
+      <Navbar bg="light" className="py-2">
         <Container>
-          <Nav className="flex-wrap justify-content-center">
-            <Nav.Link href="eyeglasses">EYE GLASSES</Nav.Link>
-            <Nav.Link href="screenglasses">SCREEN GLASSES</Nav.Link>
-            <Nav.Link href="kidglasses">KID GLASSES</Nav.Link>
-            <Nav.Link href="contactlenses">CONTACT LENSES</Nav.Link>
-            <Nav.Link href="sunglasses">SUNGLASSES</Nav.Link>
+          <Nav className="justify-content-center flex-wrap">
+            {["EYE GLASSES", "SCREEN GLASSES", "KID GLASSES", "CONTACT LENSES", "SUNGLASSES"].map((item, idx) => (
+              <Nav.Link key={idx} href={item.toLowerCase().replace(/\s+/g, "")}>
+                {item}
+              </Nav.Link>
+            ))}
           </Nav>
         </Container>
       </Navbar>
 
-      {/* Cards Section */}
-      <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", gap: "20px", padding: "20px" }}>
-        {[
-          { link: "/eyeglasses", img: "https://static1.lenskart.com/media/desktop/img/Apr22/a2.png", text: "Eyeglasses" },
-          { link: "/sunglasses", img: "https://static1.lenskart.com/media/desktop/img/Apr22/b2.png", text: "Sunglasses" },
-          { link: "/screenglasses", img: "https://static1.lenskart.com/media/desktop/img/Apr22/d2.png", text: "Screen Glasses" },
-          { link: "/contactlenses", img: "https://static1.lenskart.com/media/desktop/img/Apr22/d.png", text: "Contact Lenses" },
-          { link: "/powersunglasses", img: "https://static1.lenskart.com/media/desktop/img/Apr22/e2.png", text: "Power Sunglasses" }
-        ].map((card, index) => (
-          <Link to={card.link} key={index} style={{ textDecoration: "none", color: "inherit" }}>
-            <Card style={{ width: "16rem", height: "11rem" }}>
-              <Card.Img variant="top" src={card.img} style={{ width: "92%", margin: "auto", marginTop: "9px" }} />
-              <Card.Body>
-                <Card.Text style={{ textAlign: "center" }}>{card.text}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Link>
-        ))}
-      </div>
-
       {/* Carousel */}
-      <Carousel interval={1200} className="mt-4">
+      <Carousel className="mt-4">
         {[
           "https://static1.lenskart.com/media/desktop/img/23may/cannes/web.gif",
           "https://static1.lenskart.com/media/desktop/img/June24/combo_vc/Desktop.gif",
           "https://static1.lenskart.com/media/desktop/img/republic/petite/Web-banner%20option%202.jpg",
-          "https://static1.lenskart.com/media/desktop/img/May24/starDust/web_BannerStarDust.jpg"
+          "https://static1.lenskart.com/media/desktop/img/May24/starDust/web_BannerStarDust.jpg",
         ].map((src, idx) => (
           <Carousel.Item key={idx}>
             <img className="d-block w-100" src={src} alt={`Slide ${idx + 1}`} />
@@ -116,12 +107,29 @@ const Home1 = () => {
         ))}
       </Carousel>
 
-      {/* Other Sections */}
-      <div className="container my-5">
-        <h3 className="text-center">Free Progressive Lenses</h3>
-        <img src="https://static1.lenskart.com/media/desktop/img/2024/may/prog/New%20Web%20Banner.jpg" className="img-fluid" alt="Banner" />
+      {/* Cards Section */}
+      <div className="container mt-5">
+        <div className="d-flex justify-content-between flex-wrap">
+          {[
+            { name: "Eyeglasses", img: "https://static1.lenskart.com/media/desktop/img/Apr22/a2.png", link: "/eyeglasses" },
+            { name: "Sunglasses", img: "https://static1.lenskart.com/media/desktop/img/Apr22/b2.png", link: "/sunglasses" },
+            { name: "Screen Glasses", img: "https://static1.lenskart.com/media/desktop/img/Apr22/d2.png", link: "/screenglasses" },
+            { name: "Contact Lenses", img: "https://static1.lenskart.com/media/desktop/img/Apr22/d.png", link: "/contactlenses" },
+            { name: "Power Sunglasses", img: "https://static1.lenskart.com/media/desktop/img/Apr22/e2.png", link: "/powersunglasses" },
+          ].map((card, idx) => (
+            <Link key={idx} to={card.link} className="text-decoration-none text-dark">
+              <Card style={{ width: "16rem", height: "11rem", margin: "10px" }}>
+                <Card.Img variant="top" src={card.img} className="p-3" />
+                <Card.Body className="text-center">
+                  <Card.Text>{card.name}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Link>
+          ))}
+        </div>
       </div>
 
+      {/* Login Modal */}
       <LoginModal show={show} handleClose={handleClose} />
     </>
   );
