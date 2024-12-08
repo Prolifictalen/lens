a647beb499e494e09a937343f03896a73bbca99e
Astha Singh <95242101+Prolifictalen@users.noreply.github.com>
2024-12-08 20:41:11 +0530

    Update PowerSunglasses.js

--- src/Components/PowerSunglasses.js
+++ src/Components/PowerSunglasses.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import useCounter from './Counter';
+import useCounter from './Counter'; // Import the custom hook
 
 const PowerSunglasses = () => {
   const { counters, incrementCounter, decrementCounter } = useCounter();
+
   const cardsData = [
     {
       imgSrc: "https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/628x301/9df78eab33525d08d6e5fb8d27136e95//v/i/silver-black-grey-gradient-rimless-square-vincent-chase-met-effect-vc-s15397-c2-sunglasses_g_1011_02_02_23.jpg",
@@ -57,58 +58,73 @@ const PowerSunglasses = () => {
     fontSize: '16px',
     fontWeight: 'bold'
   };
+
+  const gridStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))',
+    gap: '20px',
+    padding: '20px'
+  };
+
   return (
     <>
       <div>
-      <img src="https://static1.lenskart.com/media/desktop/img/harmony/28-jun-24/Web%20Banner%201920x520.jpg"
-      width="100%"
-      height="240"
-      alt=""
-      />
-    </div>
- 
- 
-    {/* Cards */}
-      <div style={{ display: 'flex', justifyContent: 'space-between', padding: '30px' }}>
-        {cardsData.map((card, index) => (
-            <Card style={{ width: '18rem', height: 'auto', margin: '10px', position: 'relative' }}>
-              <FontAwesomeIcon icon={faHeart} style={{ position: 'absolute', top: '10px', right: '10px', cursor: 'pointer' }} />
-              <Card.Img variant="top" src={card.imgSrc} style={{ marginTop: '40px' }} />
-              <Card.Body style={{ textAlign: "left", marginTop: "7px"}}>
-                <Card.Text style={textStyle}>{card.text}</Card.Text>
-                <Card.Text style={textStyle}>{card.price}</Card.Text>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '10px' }}>
-                  <Button variant="outline-secondary" size="sm" onClick={() => decrementCounter(index)}>-</Button>
-                  <span style={{ margin: '0 10px' }}>{counters[index]}</span>
-                  <Button variant="outline-secondary" size="sm" onClick={() => incrementCounter(index)}>+</Button>
-                </div>
-                <Button style={{ width: "100%", marginLeft: "4px" }}>Add to Cart</Button>
-              </Card.Body>
-            </Card>
-        ))}
+        <img
+          src="https://static1.lenskart.com/media/desktop/img/harmony/28-jun-24/Web%20Banner%201920x520.jpg"
+          width="100%"
+          height="240"
+          alt="Power Sunglasses Banner"
+        />
       </div>
 
-
-      <div style={{ display: 'flex', justifyContent: 'space-between', padding: '30px' }}>
-        {cardsData1.map((card, index) => (
-            <Card style={{ width: '18rem', height: 'auto', margin: '10px', position: 'relative' }}>
-              <FontAwesomeIcon icon={faHeart} style={{ position: 'absolute', top: '10px', right: '10px', cursor: 'pointer' }} />
-              <Card.Img variant="top" src={card.imgSrc} style={{ marginTop: '40px' }} />
-              <Card.Body style={{ textAlign: "left", marginTop: "7px"}}>
+      {/* Cards Grid */}
+      <div style={gridStyle}>
+        {cardsData.map((card, index) => (
+          <Card key={index} style={{ width: '100%', position: 'relative' }}>
+            <FontAwesomeIcon
+              icon={faHeart}
+              style={{
+                position: 'absolute',
+                top: '10px',
+                right: '10px',
+                cursor: 'pointer'
+              }}
+            />
+            <Card.Img variant="top" src={card.imgSrc} style={{ marginTop: '20px' }} />
+            <Card.Body style={{ textAlign: 'left', marginTop: '7px' }}>
               <Card.Text style={textStyle}>{card.text}</Card.Text>
               <Card.Text style={textStyle}>{card.price}</Card.Text>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '10px' }}>
-                  <Button variant="outline-secondary" size="sm" onClick={() => decrementCounter(index)}>-</Button>
-                  <span style={{ margin: '0 10px' }}>{counters[index]}</span>
-                  <Button variant="outline-secondary" size="sm" onClick={() => incrementCounter(index)}>+</Button>
-                </div>
-                <Button style={{ width: "100%", marginLeft: "4px" }}>Add to Cart</Button>
-              </Card.Body>
-            </Card>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginBottom: '10px'
+                }}
+              >
+                <Button
+                  variant="outline-secondary"
+                  size="sm"
+                  onClick={() => decrementCounter(index)}
+                >
+                  -
+                </Button>
+                <span style={{ margin: '0 10px' }}>{counters[index]}</span>
+                <Button
+                  variant="outline-secondary"
+                  size="sm"
+                  onClick={() => incrementCounter(index)}
+                >
+                  +
+                </Button>
+              </div>
+              <Button style={{ width: '100%' }}>Add to Cart</Button>
+            </Card.Body>
+          </Card>
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default PowerSunglasses
+export default PowerSunglasses;
